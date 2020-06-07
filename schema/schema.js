9be84d6c3b043c42372d1b6ade74ce649308cd35
Sharan-ram <sharanram1993@gmail.com>
2020-06-07 10:36:29 +0530

    Add a new query for book

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,29 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLStringType } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLStringType },
-    name: { type: GraphQLStringType },
-    genre: { type: GraphQLStringType },
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from the db or other source
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
