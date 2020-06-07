f05ce75bef02995d25025cef81a116827a57bbe7
Sharan-ram <sharanram1993@gmail.com>
2020-06-07 13:53:03 +0530

    Set up mongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const graphqlHTTP = require("express-graphql");
+
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.use(
   "/graphql",
