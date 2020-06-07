1bc957dbe97af25c84baffc360804dfdd4a319e3
Sharan-ram <sharanram1993@gmail.com>
2020-06-07 10:45:08 +0530

    Add a resolve function to get a book by id

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Dummy data
+const books = [
+  { id: "1", name: "FHI", genre: "Self Help" },
+  { id: "2", name: "GBR", genre: "Business and Relationship" },
+  { id: "3", name: "VT", genre: "Philosophy" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -19,6 +27,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from the db or other source
+        return _.find(books, { id: args.id });
       },
     },
   },
