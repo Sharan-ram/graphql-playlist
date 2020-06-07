93e6c6abdccac8ef90b39dc4aba9f6508ae5ee3b
Sharan-ram <sharanram1993@gmail.com>
2020-06-07 18:33:06 +0530

    Make all fields mandatory using graphql non null

--- schema/schema.js
+++ schema/schema.js
@@ -9,6 +9,7 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLInt,
+  GraphQLNonNull,
   GraphQLSchema,
 } = graphql;
 
@@ -82,9 +83,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
         let book = new Book({
@@ -98,8 +99,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
         let author = new Author({
